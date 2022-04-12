9fef05937db5a776b13efcad60ce15d2fe01bc58
unknown <cleide.edificacoes@hotmail.com>
2022-04-12 17:38:55 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,73 @@
+// Exercício  de interpretação de código
+/* 1. 10
+10 5*/
+
+// 10 10 10//
+
+//1. a e b
+// let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+// let salario = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${salario/ horasTrabalhadas} por hora`)
+
+//c
+// let nome
+// let idade
+
+// console. log (typeof nome)
+// console. log (typeof idade)
+
+//d 
+// Porque não foi atribuido nenhum valor 
+
+//e
+//  let nomeDoUsuario = prompt("Qual é o seu nome?")
+//  let idadeDoUsuario = prompt("Qual é a sua idade?")
+
+// console.log(nomeDoUsuario, idadeDoUsuario)
+// console.log(typeof nomeDoUsuario)
+// console.log(typeof idadeDoUsuario)
+
+// // // F
+// // string
+// // string
+
+// //G
+// console.log(`Olá!, Meu nome é, ${nomeDoUsuario}, e eu tenho, ${idadeDoUsuario}, anos`)
+
+// //2.
+// const pergunta1 = prompt("Você está usando uma roupa azul hoje?")
+// const pergunta2 = prompt("Você gosta de café?")
+// const pergunta3 = prompt("Vocé é de Camaçari")
+
+// console.log(`Você está usando uma roupa azul hoje?- ${pergunta1}`)
+// console.log(`Você gosta de café?- ${pergunta2}`)
+// console.log(`Você é de Camaçari?- ${pergunta3}`)
+
+// 3.
+// let a = 10
+// let b = 25
+
+// c = a
+// a = b
+// b = c
+
+// // Depois de trocados, teremos o seguinte resultado:
+// console.log("O novo valor de a é", a) // O novo valor de a é 25
+// console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+const numero1 = prompt ("Digita um numero")
+const numero2 = prompt ("Digita outro numero")
+console.log("soma", Number(numero1) + Number(numero2))
+console.log("multiplicacao2", Number(numero1) * Number(numero2))
+
+
+
+
+
+
+
+
+
+
+
+
