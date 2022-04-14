d06335216dccff6c05a920af7bdd53a0d5e0d05f
unknown <cleide.edificacoes@hotmail.com>
2022-04-14 18:03:00 -0300

    operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,74 @@
+//Exercícios de interpretação de código
+/* 1. 
+a. False
+b. False
+c.True
+d. Booleano*/
+
+//2.
+
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// let segundoNumero = Number(prompt("Digite outro numero!"))
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+//A solução: O resultado do prompt estava como String para fazer a soma foi só converter para Number
+
+
+//Exercícios de escrita de código
+
+//1.
+
+// let idade = Number(prompt("Qual é a sua idade?"))
+
+// let idadeMelhorAmiga = Number(prompt("Qual é a idade de sua melhor amiga?"))
+
+// let resultado = idade >= idadeMelhorAmiga
+
+// console.log("Sua idade é maior do que a do seu melhor amigo?", resultado)
+
+//2.
+
+// let numeroPar = Number(prompt("Insira um numero par."))
+
+// let resultado = numeroPar % 2 
+
+// console.log("resto de divisao", resultado)
+
+//Todos os resultado do resto da divisao deu 0
+
+//Todos os resultados com o numero impar deu 1
+
+//3.
+
+// let idade = Number(prompt("Qual a sua idade?"))
+
+// console.log("idade em Meses", idade * 12)
+// console.log("idade em Anos", idade * 365)
+// console.log("idade em horas", idade * 365 * 24)
+
+//4.
+
+// let numero1 = Number(prompt("Insira um numero"))
+
+// let numero2 = Number(prompt("Insira outro numero"))
+
+// console.log("O primeiro numero é maior que segundo?", numero1 > numero2)
+
+// console.log("O primeiro numero é igual ao segundo?", numero1 === numero2)
+
+// console.log("O primeiro numero é divisível pelo segundo?", numero1 % numero2 === 0)
+
+// console.log("O segundo numero é divisível pelo primeiro?", numero2 % numero1 === 0)
+
+
+
+
+
+
+
+
+
+
