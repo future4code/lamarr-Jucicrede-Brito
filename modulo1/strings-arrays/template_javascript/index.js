5f195a8ceefbe2e8af3881107f08ffde29cfb263
unknown <cleide.edificacoes@hotmail.com>
2022-04-19 19:00:32 -0300

    strings-arrays

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,66 @@
+//Exercícios de interpretação de código
+//1.
+//a) undefined
+//b) null
+//c) 11
+//d) 3
+//e) removeu o 4 e adicionou o 19
+//f) 9
+
+//2.
+//SUBI NUM ÔNIBUS EM MIRROCOS 28
+
+//Exercícios de escrita de código
+//1.
+
+// const nome = prompt("Digite o seu nome")
+// const email = prompt("Qual seu email?")
+
+// const templateFrase = `O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`
+
+// console.log(templateFrase)
+
+//2.
+// const comidasPreferidas = ["pizza", "arroz", "churrasco", "feijão", "frutas"]
+
+// console.log(comidasPreferidas)
+
+// console.log("Essas são as minhas comidas preferidas")
+// console.log(comidasPreferidas [0])
+// console.log(comidasPreferidas [1])
+// console.log(comidasPreferidas [2])
+// console.log(comidasPreferidas [3])
+// console.log(comidasPreferidas [4])
+
+// const usuario = prompt("Qual sua comida preferida?")
+// comidasPreferidas [1] = usuario
+
+// console.log(comidasPreferidas)
+
+//3.
+// const listaDeTarefas = []
+
+// const primeiraTarefa = prompt("Qual a sua primeira tarefa do dia?")
+// const segundaTarefa = prompt("E a segunda?")
+// const terceiraTarefa = prompt("E a terceira?")
+// listaDeTarefas.push(primeiraTarefa)
+// listaDeTarefas.push(segundaTarefa)
+// listaDeTarefas.push(terceiraTarefa)
+
+// console.log(listaDeTarefas)
+
+// const perguntaUsuario = prompt("Digite um indice de uma tarefa que você já realizou 1, 2 ou 3")
+// listaDeTarefas.splice(Number(perguntaUsuario), 2)
+// console.log(listaDeTarefas)
+
+//- 🏅  Desafios
+//1.
+// const frase = "Pro dia nascer feliz"
+// const resultado = frase.split(" ")
+// console.log(resultado)
+
+//2.
+// const listaCompras = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+// console.log("tamanho da array:", listaCompras.length)
+// console.log("índice da palavra Abacaxi:", listaCompras.indexOf('Abacaxi'))
+
