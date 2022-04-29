eb667987c9c0af7871dd6a41eeb7af7a5b27c167
unknown <cleide.edificacoes@hotmail.com>
2022-04-28 21:36:07 -0300

    Comecei mas nao conseguir finalizar o terceiro do desafio apaguei

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,87 @@
+//Exercícios de interpretação de código
+//1.
+//a) Testa se os numeros são pares
+//b) Todos os numeros pares imprime no console "Passou no teste"
+//c)Todos os numeros impares a mensagem é "Não passou no teste"
+
+//2.
+//a) Para dizer 
+//b) O preço da macã  é  2.25 reais
+//c) O preço da pera é 5.5 reais
+
+//3. 
+//a) Pedindo ao usuario para digitar um numero
+//b) Numero 10, Esse número passou no teste. -10 indefinido
+//c)Sim, pois no lugar do let deveria ser o else de comparação
+
+//Exercícios de escrita de código
+
+//1.
+// const idadeUsuario = Number (prompt("Qual a sua idade?"))
+
+// if (idadeUsuario > 18){
+//     console.log("Você pode dirigir.")
+// } else{
+//     console.log("Você não pode dirigir.")
+// }
+
+//2.
+// const turnoDoAluno = prompt('Qual turno do dia seu filho estuda? Por favor insira somente a inicial sendo M (matutino), V (Vespertino) ou N (Noturno)')
+
+// if (turnoDoAluno === "M"){
+//     console.log("Bom dia!")
+// }else{
+//     if(turnoDoAluno === "V"){
+//     console.log("Boa tarde!")}
+//     else{
+//         console.log("Boa noite!")
+//     }
+// }
+
+//3.
+// const turnoDoAluno = prompt('Qual turno seu filho estuda? Por favor insira somente a inicial sendo M (matutino), V (Vespertino) ou N (Noturno)')
+
+// switch (turnoDoAluno) {
+//     case "M":
+//         console.log("Bom dia!")
+
+//         break;
+
+//     case "V":
+//         console.log("Boa tarde!")
+
+//         break;
+
+//     case "N":
+//         console.log("Boa noite!")    
+
+//         break;
+
+// //     default:
+
+// }
+
+//4.
+// const genero = prompt("Qual o genero do filme que vão assistir?")
+// const preco = prompt("Qual o valor de ingresso?")
+
+// if (genero === "fantasia" && preco<=15){
+//     console.log("Bom filme!")
+// }else{
+//     console.log("Escolha outro filme :(")
+// }
+
+//🏅  Desafios
+//1.
+// const genero = prompt("Qual o genero do filme que vão assistir?")
+// const preco = prompt("Qual o valor de ingresso?")
+// const lanchinho = prompt(" Qual lanchinho você vai comprar (pipoca, chocolate, doces, etc)")
+
+// if (genero === "fantasia" && preco<=15){
+//     console.log("Bom filme!")
+//     console.log(`Aproveite seu(a) ${lanchinho}`)
+// }else{
+//     console.log("Escolha outro filme :(")
+// }
+
+
