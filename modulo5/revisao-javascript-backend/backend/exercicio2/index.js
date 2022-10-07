0c2fffe6accc3e166d0358df629f935b3135bb03
unknown <cleide.edificacoes@hotmail.com>
2022-10-06 22:30:09 -0300

    revisao

--- modulo5/revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,1 +1,29 @@
-console.log("exercício 2");
+const listaClientes = [
+    {id: 1, nome:"Fulano"},
+    {id: 2, nome:"Ciclano"},
+    {id: 3, nome:"Beltrano"},
+    {id: 4, nome:"Fulana"},
+]
+
+const listaCadastro = (id,nome) => {
+
+    let novaLista = [].concat(listaClientes)
+    novaLista.push({id,nome})
+    console.log(novaLista);
+
+}
+listaCadastro(5, "Juci")
+
+
+
+// Geração de tabuada
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const tabuada = () => {
+    for (let i=0; 1<10; i++)
+         
+    return numeros*i
+
+}
+console.log (tabuada)
