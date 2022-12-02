b682e10d3ff6aca6e151627d59fee0f2b61344e2
unknown <cleide.edificacoes@hotmail.com>
2022-12-01 23:00:47 -0300

    Revisão

---
+++ modulo5/revisao-mod5/src/endpoints/createCharacter.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { characters } from "../data";
+
+export function createCharacter (req:Request, res:Response): void{
+  
+    const {name, gender, description} = req.body
+
+    console.log(req.headers["content-type"]);
+    
+
+    characters.push({
+        id: Date.now(),
+        name,
+        gender,
+        description
+    })
+  
+    res.status(201).end()
+}
---
+++ modulo5/revisao-mod5/src/types.ts
@@ -0,0 +1,16 @@
+export type movie ={
+    id: number,
+    title:string,
+    year:number
+}
+export enum GENDER {
+    MALE = "MALE",
+    FEMALE = "FEMALE",
+    OTHER = "OTHER"
+}
+export type character = {
+    id: number,
+    name: string,
+    gender: GENDER,
+    description?: string
+}
