ff5c772ece83eee17b65cca39a094d503d63d7b4
unknown <cleide.edificacoes@hotmail.com>
2023-03-07 22:15:09 -0300

    test

---
+++ modulo6/testes-automatizados/tests/balancegreaterthan.test.ts
@@ -0,0 +1,42 @@
+import { User } from "../src/funcs/models/user";
+import { performPurchase } from "../src/funcs/performePurchase"
+
+
+test("Testing balance greater than value", () => {
+    const user: User = {
+        name: "Astrodev",
+        balance: 100
+    }
+
+    const result = performPurchase(user, 40)
+    
+    expect(result).toEqual({
+        name: "Astrodev",
+        balance: 60
+    })
+});
+
+test("Testing balance greater than value", () => {
+    const user: User = {
+        name: "Astrodev",
+        balance: 50
+    }
+
+    const result = performPurchase(user, 50)
+    
+    expect(result).toEqual({
+        ...user,
+        balance: 0
+    })
+});
+
+test("Testing balance greater than value", () => {
+    const user: User = {
+        name: "Astrodev",
+        balance: 30
+    }
+
+    const result = performPurchase(user, 50)
+    
+    expect(result).not.toBeDefined()
+})
